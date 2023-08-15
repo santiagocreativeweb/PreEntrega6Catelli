a8914712e8dc56878704bed46c8105a4c9d8579e
santiagocreativeweb <santiagomcatelli@gmail.com>
2023-08-15 20:18:25 -0300

    Initial commit

---
+++ src/Global/Colors.js
@@ -0,0 +1,17 @@
+export const colors = {
+    red: "#FF6666",
+    peach: "#FF8989",
+    pink: "#FCAEAE",
+    lightPink: "#FFEADD",
+    header: "#393547",
+    headerchill: "#564F6C",
+    white: "#fff",
+    black: "#000000",
+}
+
+/* export const colors = {
+    red: "#FF6666",
+    peach: "#FF8989",
+    pink: "#FCAEAE",
+    lightPink: "#FFEADD"
+} */
---
+++ src/Screens/Home.jsx
@@ -0,0 +1,37 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { colors } from '../Global/Colors'
+import CategoryItem from '../Components/CategoryItem'
+import { useGetCategoriesQuery } from '../Services/shopServices'
+
+const Home = ({
+  navigation
+}) => {
+  const {data: categories, isLoading, isError} = useGetCategoriesQuery()
+  
+  return (
+    <View style={styles.container}>
+        <FlatList
+            data = {categories}
+            keyExtractor={category => category}
+            renderItem={({item}) => <CategoryItem item={item} navigation = {navigation}/>}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.wrapper}
+            horizontal={true}
+            style={styles.flatlist}
+        />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    wrapper: {
+      gap: 20,
+    },
+    container: {
+        alignItems: 'center',
+    },
+})
